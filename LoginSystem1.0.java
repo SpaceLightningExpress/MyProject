934e80e9a6d645cbc93dc6b84e1c98051829991f
SpaceLightningExpress <132036383+SpaceLightningExpress@users.noreply.github.com>
2024-02-07 20:38:58 +0800

    Fixed Login System when u fail it show error logs
    Just make it equals on string will fix this error

--- LoginSystem1.0.java
+++ LoginSystem1.0.java
@@ -7,18 +7,21 @@ public class SimpleLoginSystem {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter Your Username1");
-        String Username = scanner.next("Username1");
+        String Username = scanner.next();
 
         System.out.println("Enter Your Password1515");
-        String Password = scanner.next("Password1515");
+        String Password = scanner.next();
 
         scanner.close();
 
-            System.out.println("Loading");
-            Thread.sleep(2500);
+        System.out.println("Loading");
+        Thread.sleep(2500);
 
+        if (Password.equals("Password1515") && Username.equals("Username1")) {
             System.out.println("Correct!!\n" + Username + "\n" + Password);
-
+    }else{
+            System.out.println("Wrong!!");
+        }
     }
 }
 
